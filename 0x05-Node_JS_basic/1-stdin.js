8c4169a3afc1a137bff7e9aee280b7d62e52b813
Akin <ogundareakinniyi8@gmail.com>
2024-10-09 17:45:45 +0100

    added 1-stdin.js

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+process.stdin.setEncoding('utf-8');
+
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
+process.on('exit', () => {
+  console.log('This important software is now closing');
+});
